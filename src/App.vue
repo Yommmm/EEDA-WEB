77247910232f63cb71efd2525280a3a899cead3f
杨智文 <yangzhiwen@chalco-steering.com>
2019-04-30 14:29:38 +0800

    加入axios

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,56 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <router-view/>
+    <!-- <router-view/> -->
+    <h1>设备配置管理</h1>
+    <h1>数据可视化</h1>
+    <h1>其他管理</h1>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
-  name: 'App'
-}
+  name: "App",
+
+  components: {},
+
+  data: function() {
+    return {
+      res: {}
+    };
+  },
+
+  computed: {},
+  watch: {},
+  methods: {
+    testAxios() {
+      var instance = axios.create({
+        baseURL: "http://39.108.83.208:9879",
+        timeout: 1000,
+      });
+      instance
+        .get("/eeda/v1/equipmentData/list")
+        .then(function(response) {
+          console.log(response.data);
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
+    }
+  },
+
+  mounted() {
+    console.log("6666666666");
+    this.testAxios();
+  }
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
